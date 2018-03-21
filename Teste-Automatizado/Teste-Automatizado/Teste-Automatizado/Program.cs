c537df34a63ffe2e87fadc08c6ee39889eec7e18
Odair <odair.santiago@prodaptech.com.br>
2018-03-21 16:08:21 -0300

    Vídeo 1

---
+++ Teste-Automatizado/Teste-Automatizado/Teste-Automatizado/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Firefox;
+using OpenQA.Selenium.Chrome;
+
+namespace Teste_Automatizado
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            IWebDriver driver = new FirefoxDriver();
+            driver.Navigate().GoToUrl("https://www.google.com.br/");
+
+            IWebElement campoDeTexto =  driver.FindElement(By.Name("q"));
+            campoDeTexto.SendKeys("Caelum");
+
+            campoDeTexto.Submit();
+            driver.Close();
+        }
+    }
+}
