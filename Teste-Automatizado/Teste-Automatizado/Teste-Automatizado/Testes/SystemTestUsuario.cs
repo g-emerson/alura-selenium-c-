7a7ee1921d6d3f59dd40638c61c72d6037153151
Odair <odair.santiago@prodaptech.com.br>
2018-03-22 13:52:02 -0300

    Testando usuário sem nome

--- Teste-Automatizado/Teste-Automatizado/Teste-Automatizado/Testes/SystemTestUsuario.cs
+++ Teste-Automatizado/Teste-Automatizado/Teste-Automatizado/Testes/SystemTestUsuario.cs
@@ -36,5 +36,30 @@ namespace Teste_Automatizado.Testes
 
             driver.Close();
         }
+
+        [Test]
+        public void deveValidarNomeCadastrarUsuario()
+        {
+            IWebDriver driver = new FirefoxDriver();
+
+            driver.Navigate().GoToUrl("http://localhost:8080/usuarios/new");
+
+            IWebElement campoNome = driver.FindElement(By.Name("usuario.nome"));
+            IWebElement campoEmail = driver.FindElement(By.Name("usuario.email"));
+            IWebElement btnSalvar = driver.FindElement(By.Id("btnSalvar"));
+
+            campoNome.SendKeys("");
+            campoEmail.SendKeys("");
+
+            btnSalvar.Click();
+
+            bool mensagemNome = driver.PageSource.Contains("Nome obrigatorio!");
+            bool mensagemEmail = driver.PageSource.Contains("E-mail obrigatorio!");
+
+            Assert.IsTrue(mensagemNome);
+            Assert.IsTrue(mensagemEmail);
+
+            driver.Close();
+        }
     }
 }
