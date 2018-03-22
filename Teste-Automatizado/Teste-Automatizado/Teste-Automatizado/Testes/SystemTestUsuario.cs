c00edfdc46c866d46b5219a2f7a1bf14eb41437f
Odair <odair.santiago@prodaptech.com.br>
2018-03-22 13:45:27 -0300

    02 Preenchendo e testando formulários

---
+++ Teste-Automatizado/Teste-Automatizado/Teste-Automatizado/Testes/SystemTestUsuario.cs
@@ -0,0 +1,40 @@
+﻿using OpenQA.Selenium;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using OpenQA.Selenium.Firefox;
+using NUnit.Framework;
+
+namespace Teste_Automatizado.Testes
+{
+    [TestFixture]
+    class SystemTestUsuario
+    {
+        [Test]
+        public void deveCadastrarUsuario()
+        {
+            IWebDriver driver = new FirefoxDriver();
+
+            driver.Navigate().GoToUrl("http://localhost:8080/usuarios/new");
+
+            IWebElement campoNome = driver.FindElement(By.Name("usuario.nome"));
+            IWebElement campoEmail = driver.FindElement(By.Name("usuario.email"));
+            IWebElement btnSalvar = driver.FindElement(By.Id("btnSalvar"));
+
+            campoNome.SendKeys("Usuario 1 nome");
+            campoEmail.SendKeys("email@user.com");
+
+            btnSalvar.Click();
+
+            bool achouNome = driver.PageSource.Contains("Usuario 1 nome");
+            bool achouEmail = driver.PageSource.Contains("email@user.com");
+
+            Assert.IsTrue(achouNome);
+            Assert.IsTrue(achouEmail);
+
+            driver.Close();
+        }
+    }
+}
